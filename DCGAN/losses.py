2b4638d379cac0707f9875b9f239466f121bde41
Jacob Thrasher <58013394+j0ngle@users.noreply.github.com>
2021-11-18 13:34:02 -0500

    started loss functions

---
+++ DCGAN/losses.py
@@ -0,0 +1,22 @@
+import tensorflow as tf
+from tensorflow.math import log, sigmoid
+from tensorflow import keras
+
+def minimax_loss_D(real_output, fake_output, apply_smoothing=True, apply_noise=True):
+  '''
+  Discriminator wants to MAXIMIZE this function, so we should take the negative
+  Ex[log(D(x))] + Ez[log(1-D(G(z)))], where:
+      Ex = expected value over all real data instances (1s)
+      D(x) = Discriminator's estimate of the probability that (real) x is real
+      Ez   = expected value over all fake data instances (0s)
+      G(z) = Generator's outputs
+      D(G(z)) = Discriminator's estimate of the probability that (fake) z is 
+  '''
+
+  Ex = tf.ones_like(real_output)
+  real_loss = -Ex * tf.math.log(real_output)
+
+  Ez = tf.zeros_like(fake_output)
+  fake_loss = -Ez * tf.math.log(1 - fake_output)
+  
+  return real_loss + fake_loss
