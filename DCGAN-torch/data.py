469d001a036e6560feadca1fa3b23e8cb390f97e
Jacob Thrasher <jthrasher0100@gmail.com>
2022-04-28 10:05:05 -0400

    Fixed FID score

--- DCGAN-torch/data.py
+++ DCGAN-torch/data.py
@@ -8,6 +8,7 @@ import numpy as np
 from PIL import Image
 from network import Identity
 from network import IMG_SIZE
+from scipy.linalg import sqrtm
 
 inception = inception_v3(pretrained=True)
 inception.fc = Identity()
@@ -76,13 +77,37 @@ def compute_fid(real_embeddings, fake_embeddings):
     C_r = torch.cov(real_embeddings)   
     C_f = torch.cov(fake_embeddings)
     C_mean = torch.sqrt(torch.mm(C_r, C_f))
-
+    
     if torch.is_complex(C_mean):
         C_mean = torch.real(C_mean)
 
     trace = torch.trace(C_r + C_f - 2*C_mean)
 
     return (sq_norm + trace).item()
+
+#TODO: The real emeddings are computer on the same image set
+#each time. This can be optimized by computing them before training
+def compute_fid_numpy(real_images, fake_images):
+    real_images = fid_resize(real_images)
+    fake_images = fid_resize(fake_images)
+
+    real_embeddings = inception(real_images).detach().numpy()
+    fake_embeddings = inception(fake_images).detach().numpy()
+
+    mu_real = real_embeddings.mean(axis=0)
+    mu_fake = fake_embeddings.mean(axis=0)
+    sq_norm = np.sum((mu_real - mu_fake) ** 2)
+
+    C_r = np.cov(real_embeddings, rowvar=False)
+    C_f = np.cov(fake_embeddings, rowvar=False)
+    C_mean = sqrtm(C_r.dot(C_f))
+
+    if np.iscomplexobj(C_mean):
+        C_mean = C_mean.real
+
+    trace = np.trace(C_r + C_f - 2*C_mean)
+
+    return sq_norm + trace
 
 
 
